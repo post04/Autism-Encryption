8d498954de8be09b92acd85117da717860ebdc73
dragon script <agomila96@gmail.com>
2022-04-11 20:42:30 +0000

    Add a few comments

--- autism.go
+++ autism.go
@@ -6,9 +6,11 @@ import (
 	"strings"
 )
 
+// Why expect a string and return a byte?
 func AutismEncrypt(text string) []byte {
+	// What does this even mean?
 	shatext := fmt.Sprintf("%X", sha256.Sum256([]byte(text)))
-	// TODO: add all emojis
+	// TODO: add all emojis (see git history & my commit messages)
 	var key map[string]string = map[string]string{
 		"a": "😜",
 		"b": "🤪",
@@ -16,9 +18,11 @@ func AutismEncrypt(text string) []byte {
 		"d": "", //...
 	}
 	var output = ""
+	// this could be reduced even more
 	for _, char := range shatext {
 		autistiChar := key[string(char)]
 		output += strings.ReplaceAll(autistiChar, "\"", "")
 	}
+	// Should use strings
 	return []byte(output)
 }
